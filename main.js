fe3a82a4a047213caa690ea68269892870ac9d17
deejayosamu <seogwangwon1129@gmail.com>
2023-11-11 23:01:53 +0100

    create project

---
+++ main.js
@@ -0,0 +1,7 @@
+import Dropdown from "bootstrap/js/dist/dropdown";
+import Modal from "bootstrap/js/dist/modal"
+
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+dropdownElementList.map(dropdownToggleEl => new Dropdown(dropdownToggleEl))
+
+const myModal = new Modal(document.querySelector('#exampleModal'), options)
